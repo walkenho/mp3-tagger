e833de29dc63c885d4fd86d8727b78da79e4943f
Jessica Walkenhorst <walkenho@gmail.com>
2020-12-08 00:26:11 +0000

    Add ability to set genre

--- src/musictagger.py
+++ src/musictagger.py
@@ -3,6 +3,8 @@ from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH, MP3OpenFileE
 import pandas as pd
 from typing import List
 
+from mutagen.easyid3 import EasyID3
+
 BASEPATH = Path('/media/walkenho/Seagate Backup Plus Drive/Eigene Musik')
 
 
@@ -28,83 +30,43 @@ def find_all_mp3s(path: Path) -> List[Path]:
 def tag_song(filepath: Path, **attrs) -> None:
     """
     Allowed tags:
-
-    - artist;
-    - album;
-    - song;
-    - track;
-    - comment;
-    - year;
-    - genre;
-    - band (version 2.x);
-    - composer (version 2.x);
-    - copyright (version 2.x);
-    - url (version 2.x);
-    - publisher (version 2.x)
+    * 'album'
+    * 'artist'
+    * 'albumartist'
+    * 'date'
+    * 'discnumber'
+    * 'tracknumber'
+    * 'genre'
+    * 'language'
+    * 'bpm', 'compilation',
+    'composer', 'copyright', 'encodedby', 'lyricist', 'length', 'media', 'mood',
+    'title', 'version', 'conductor', 'arranger',  'organization',
+    'author', 'albumartistsort', 'albumsort', 'composersort', 'artistsort', 'titlesort',
+    'isrc', 'discsubtitle', 'originaldate', 'performer:*', 'musicbrainz_trackid',
+    'website', 'replaygain_*_gain', 'replaygain_*_peak', 'musicbrainz_artistid', 'musicbrainz_albumid',
+    'musicbrainz_albumartistid', 'musicbrainz_trmid', 'musicip_puid', 'musicip_fingerprint',
+    'musicbrainz_albumstatus', 'musicbrainz_albumtype', 'releasecountry', 'musicbrainz_discid',
+    'asin', 'performer', 'barcode', 'catalognumber', 'musicbrainz_releasetrackid',
+     'musicbrainz_releasegroupid', 'musicbrainz_workid', 'acoustid_fingerprint', 'acoustid_id'
     """
 
-    mp3 = MP3File(str(BASEPATH / filepath))
-    mp3.set_version(VERSION_BOTH)
+    audio = EasyID3(BASEPATH / filepath)
 
     for k, v in attrs.items():
-        if k == 'track':
-            mp3.set_version(VERSION_1)
-            track_number = v.split('/')[0]
-            setattr(mp3, k, track_number)
+        audio[k] = v
 
-            mp3.set_version(VERSION_2)
-            setattr(mp3, k, v)
-
-            mp3.set_version(VERSION_BOTH)
-        elif k == 'genre':
-            mp3.set_version(VERSION_2)
-            setattr(mp3, k, v)
-            mp3.set_version(VERSION_BOTH)
-        else:
-            try:
-                if pd.isnull(v):
-                    if k == 'comment':
-                        del mp3.comment
-                    elif k == 'copyright':
-                        del mp3.copyright
-                    elif k == 'url':
-                        del mp3.url
-                    else:
-                        setattr(mp3, k, '')
-                else:
-                    setattr(mp3, k, v)
-            except AttributeError:
-                print(filepath, k, v)
-                raise AttributeError
-
-    mp3.save()
+    audio.save()
 
 
-def get_table(mypath: Path, mode: str) -> pd.DataFrame:
-    data = []
-    for f in find_all_mp3s(mypath):
-        try:
-            tags = MP3File(str(BASEPATH / f)).get_tags()
-            if mode in ['v1', 'both']:
-                d1 = {k + '.v1': v for k, v in tags['ID3TagV1'].items()}
-            else:
-                d1 = {}
-            if mode in ['v2', 'both']:
-                d2 = {k + '.v2': v for k, v in tags['ID3TagV2'].items()}
-            else:
-                d2 = {}
-            data.append({'filename': f, **d1, **d2})
-        except MP3OpenFileError:
-            print(f"{f} is not in MP3 format")
-
-    df = pd.DataFrame.from_dict(data)
-    df = df[sorted(df.columns)]
+def get_table(path: Path) -> pd.DataFrame:
+    df = pd.DataFrame.from_dict({})
+    for f in find_all_mp3s(path):
+        df = df.append(pd.DataFrame.from_dict({**dict(EasyID3(BASEPATH/f)), 'filename': f}))
     return df
 
 
-def get_tags_from_v2(df: pd.DataFrame) -> dict:
+def get_tags(df: pd.DataFrame) -> dict:
     dfc = df.copy()
-    dfc.columns = [c.split('.v2')[0] for c in dfc.columns]
     dfc.set_index('filename', inplace=True)
     d = dfc.to_dict('index')
     return d
@@ -116,43 +78,56 @@ def retag(mydict: dict) -> None:
 
 
 def delete_copyright(df: pd.DataFrame):
-    df['copyright.v2'] = None
+    df['copyright'] = None
 
 
 def delete_url(df: pd.DataFrame):
-    df['url.v2'] = None
+    df['url'] = None
 
 
 def delete_comment(df: pd.DataFrame):
-    df['comment.v2'] = None
+    df['comment'] = None
 
 
 def extract_track_number(mystr):
     return int(str(mystr).split('/')[0])
 
 
-def create_new_track_nr(track_int, artist, album, disk_int, mydict):
-    return f"{track_int}/{mydict[(artist, album, disk_int)]}"
+def create_new_track_nr(track_int, artist, album, disc_int, mydict):
+    return f"{track_int:02}/{mydict[(artist, album, disc_int)]}"
 
 
-def create_new_disk_nr(disk_int, artist, album, mydict):
-    return f"{disk_int}/{mydict[(artist, album)]}"
+def create_new_disc_nr(disc_int, artist, album, mydict):
+    return f"{disc_int}/{mydict[(artist, album)]}"
 
 
-def set_track_and_disk_number(df):
-    if 'disk.v2' not in df.columns:
-        df['disk_int'] = 1
+def set_track_and_disc_number(df):
+    if 'discnumber' not in df.columns:
+        df['disc_int'] = 1
     else:
-        df['disk_int'] = df['disk.v2'].map(lambda x: extract_track_number(x))
+        df['discnumber'] = df['discnumber'].fillna(1)
+        df['disc_int'] = df['discnumber'].map(lambda x: extract_track_number(x))
 
-    df['track_int'] = df['track.v2'].map(lambda x: extract_track_number(x))
+    df['track_int'] = df['tracknumber'].map(lambda x: extract_track_number(x))
 
-    max_disk_dict = df.groupby(['artist.v2', 'album.v2']).max('disk_int').to_dict()['disk_int']
-    max_track_dict = df.groupby(['artist.v2', 'album.v2', 'disk_int']).max('track_int').to_dict()['track_int']
+    if 'albumartist' not in df.columns:
+        df['albumartist'] = df['artist']
+        del_albumartist = True
+    else:
+        del_albumartist = False
 
-    df['track.v2'] = df[['track_int', 'artist.v2', 'album.v2', 'disk_int']].apply(
+    max_disc_dict = df.groupby(['albumartist', 'album']).max('disc_int').to_dict()['disc_int']
+    max_track_dict = df.groupby(['albumartist', 'album', 'disc_int']).max('track_int').to_dict()['track_int']
+
+    df['tracknumber'] = df[['track_int', 'albumartist', 'album', 'disc_int']].apply(
         lambda x: create_new_track_nr(x[0], x[1], x[2], x[3], max_track_dict), axis=1)
-    df['disk.v2'] = df[['disk_int', 'artist.v2', 'album.v2']].apply(
-        lambda x: create_new_disk_nr(x[0], x[1], x[2], max_disk_dict), axis=1)
+    df['discnumber'] = df[['disc_int', 'albumartist', 'album']].apply(
+        lambda x: create_new_disc_nr(x[0], x[1], x[2], max_disc_dict), axis=1)
 
-    df.drop(['track_int', 'disk_int'], axis=1, inplace=True)
+    df.drop(['track_int', 'disc_int'], axis=1, inplace=True)
+    if del_albumartist:
+        df.drop(['albumartist'], axis=1, inplace=True)
+
+
+def extract_possible_genres(df):
+    return list(df['genre'].drop_duplicates().values)
