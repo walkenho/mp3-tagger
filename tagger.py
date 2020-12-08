a6888dbcc29ca44f2b37d526676607b2529ac3e8
Jessica Walkenhorst <walkenho@gmail.com>
2020-12-08 00:16:00 +0000

    Change to using mutagen instead of mp3tagger

--- tagger.py
+++ tagger.py
@@ -3,9 +3,14 @@ import string
 import streamlit as st
 
 from musictagger import BASEPATH, find_artists, get_table, delete_comment, delete_url, delete_copyright, \
-    get_tags_from_v2, retag, find_albums_for_artist
+    retag, find_albums_for_artist, set_track_and_disc_number, get_tags, extract_possible_genres
 
 st.title("Welcome to the Music Tagger")
+st.markdown("""## Instructions:
+1. Select the Albums you would like to retag.
+2. Select the procedures to apply from the sidebar.
+3. Check the results.
+4. If you are happy with the results, press Save.""")
 
 artists = find_artists()
 
@@ -18,30 +23,43 @@ album = st.selectbox("Select Album", ["None", "Any"] + [a for a in find_albums_f
 if artist != "None" and album != "None":
     # make table
     if album == "Any":
-        df = get_table(BASEPATH / artist, 'v2')
+        df = get_table(BASEPATH / artist)
     else:
-        df = get_table(BASEPATH/artist/album, 'v2')
+        df = get_table(BASEPATH/artist/album)
 
     st.write("Old Tags:")
     st.write(df)
 
-    delete_comment_box = st.checkbox('Delete Comments')
+    delete_comment_box = st.sidebar.checkbox('Delete Comments')
     if delete_comment_box:
         delete_comment(df)
 
-    delete_url_box = st.checkbox('Delete URL')
+    delete_url_box = st.sidebar.checkbox('Delete URL')
     if delete_url_box:
         delete_url(df)
 
-    delete_copyright_box = st.checkbox('Delete Copyright')
+    delete_copyright_box = st.sidebar.checkbox('Delete Copyright')
     if delete_copyright_box:
         delete_copyright(df)
+
+    renumber_tracks_box = st.sidebar.checkbox('Update Tracks and Disc numbers')
+    if renumber_tracks_box:
+        set_track_and_disc_number(df)
+
+    update_genre_box = st.sidebar.checkbox("Update Genre")
+    if update_genre_box:
+        new_genre_selection = st.sidebar.selectbox('Update Genre', options=extract_possible_genres(df) + ['Other'])
+        if new_genre_selection == 'Other':
+            new_genre = st.sidebar.text_input('Provide the name for the new Genre', '')
+        else:
+            new_genre = new_genre_selection
+        df['genre'] = new_genre
 
     st.write("New Tags:")
     st.write(df)
 
     save_button = st.button("Save")
     if save_button:
-        tags = get_tags_from_v2(df)
-        #retag(tags)
+        tags = get_tags(df)
+        retag(tags)
         st.balloons()
