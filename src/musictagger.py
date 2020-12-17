a5c2bca6b7493488f6cc38720629c51ca0435a98
Jessica Walkenhorst <walkenho@gmail.com>
2020-12-17 00:43:23 +0000

    Add basic reporting functionality

--- src/musictagger.py
+++ src/musictagger.py
@@ -4,8 +4,10 @@ import mutagen
 import pandas as pd
 from typing import List
 
+from mutagen.asf import ASFError
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3NoHeaderError
+from mutagen.mp3 import HeaderNotFoundError, error
 
 BASEPATH = Path('/media/walkenho/Seagate Backup Plus Drive/Eigene Musik')
 
@@ -17,13 +19,14 @@ ENCODEDBY = 'encodedby'
 COPYRIGHT = 'copyright'
 TRACKNUMBER = 'tracknumber'
 
-
-def find_artists() -> List[Path]:
+def find_categories() -> List[Path]:
     return [p.relative_to(BASEPATH) for p in BASEPATH.glob("*")]
 
+def find_artists_for_category(category:str) -> List[Path]:
+    return [p.relative_to(BASEPATH/category) for p in (BASEPATH/category).glob("*")]
 
-def find_albums_for_artist(artist: str) -> List[Path]:
-    return [p.relative_to(BASEPATH/artist) for p in (BASEPATH/artist).glob("*")]
+def find_albums_for_category_and_artist(category: str, artist: str) -> List[Path]:
+    return [p.relative_to(BASEPATH/category/artist) for p in (BASEPATH/category/artist).glob("*")]
 
 
 def find_all_mp3s(path: Path) -> List[Path]:
@@ -36,13 +39,27 @@ def find_all_mp3s(path: Path) -> List[Path]:
         lst.append(path.relative_to(BASEPATH))
     return lst
 
+
 def load_mp3(path: Path):
     try:
         audio = EasyID3(path)
-    except ID3NoHeaderError:
-        audio = mutagen.File(path, easy=True)
-        audio.add_tags()
-    return audio
+        return audio
+    except (ID3NoHeaderError):
+        try:
+            audio = mutagen.File(path, easy=True)
+            audio.add_tags()
+            return audio
+        except HeaderNotFoundError:
+            print(f"Header Not Found error for file {path} - Not  valid MP3 file")
+        except ASFError:
+            print(f"ASFError for file {path}")
+    except Exception as err:
+        if err.args[0] == "Header size not synchsafe":
+            print(f"Encountered Header Size not synchsafe error in file {path}")
+        else:
+            print(f"Undefined error detected {err}")
+            raise err
+
 
 def tag_song(filepath: Path, **attrs) -> None:
     """
@@ -83,10 +100,11 @@ def get_table(path: Path) -> pd.DataFrame:
     df = pd.DataFrame()
     for f in find_all_mp3s(path):
         audio = load_mp3(BASEPATH/f)
-        if not dict(audio).keys():
-            df = df.append(pd.DataFrame.from_dict({'filename': [f]}))
-        else:
-            df = df.append(pd.DataFrame.from_dict({**dict(audio), 'filename': f}))
+        if audio:
+            if not dict(audio).keys():
+                df = df.append(pd.DataFrame.from_dict({'filename': [f]}))
+            else:
+                df = df.append(pd.DataFrame.from_dict({**dict(audio), 'filename': f}))
     return df
 
 
@@ -164,3 +182,49 @@ def set_combined_disc_number(df: pd.DataFrame) -> None:
 
 def extract_options(df, column):
     return list(df[column].drop_duplicates().values)
+
+
+def collect_data(path):
+    results = []
+    skip_counter = 0
+    untagged_counter = 0
+    files_total = 0
+    for f in find_all_mp3s(path):
+        files_total = files_total + 1
+        audio = load_mp3(BASEPATH/f)
+        if audio is not None:
+            results.append({**{k: v[0] for k, v in {**dict(audio)}.items()}, 'filename': BASEPATH / f})
+            if not audio:
+                untagged_counter = untagged_counter + 1
+        else:
+            skip_counter = skip_counter + 1
+    return results, skip_counter, untagged_counter, files_total
+
+
+if __name__ == '__main__':
+    results = []
+    skip_counter = 0
+    untagged_counter = 0
+    files_counter = 0
+    folders = (BASEPATH).glob('*')
+    for folder in folders:
+        res, c, t, f = collect_data(BASEPATH/folder)
+        print(folder)
+        results = results + res
+        skip_counter = skip_counter + c
+        untagged_counter = untagged_counter + t
+        files_counter = files_counter + f
+        if c != 0:
+            print(f"{c} files skipped in this folder. Now {skip_counter} files skipped in total.")
+        if t !=0:
+            print(f"{t} files completely untagged. Now {untagged_counter} completely untagged.")
+
+    print(f"{files_counter} mp3s analyzed")
+    print(f"{len(results)} mp3s were added to table")
+    print(f"{skip_counter} files skipped")
+    print(f"{files_counter} should be equal to {len(results) + skip_counter}")
+    print(f"{untagged_counter} completely untagged")
+
+
+    #TODO: Sth here isn't right yet. A lot of files seem to get skipped, but don't error. How can this be?
+    #TODO: Use example folder for Aventura (for example).
