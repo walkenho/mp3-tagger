b7c3af79cdb68819a690d8b02bd265d4fc3e5cad
Jessica Walkenhorst <walkenho@users.noreply.github.com>
2021-07-22 23:41:22 +0100

    Feature/slimline tagger (#3)
    * Move interface functionality into its own module
    
    * Streamline more
    
    * Streamline sidemenu built
    
    * Slimline

--- tagger.py
+++ tagger.py
@@ -3,7 +3,7 @@ import streamlit as st
 from musictagger.core import get_table, delete_column, \
     retag, get_tags, \
     DISCNUMBER, ALBUM, GENRE, ARTIST, ALBUMARTIST, ENCODEDBY, COPYRIGHT, set_combined_track_number, \
-    set_combined_disc_number, TITLE, DATE, \
+    set_combined_disc_number, TITLE, DATE, COMPOSER,\
     TRACKNUMBER, LANGUAGE, add_albumart, LENGTH, BASEPATH
 from musictagger.interface import get_albumpath_from_interface, update_based_on_previous_value
 
@@ -32,6 +32,13 @@ if folder:
                 st.warning(f"{category}: Multiple Entries found [{(', ').join(category_values)}]")
                 problem_counter = problem_counter + 1
         else:
+            st.warning(f"{category}: Tags not found")
+
+    for category in tagtable.columns:
+        if category not in [ALBUM, ARTIST, ALBUMARTIST, GENRE, LANGUAGE, DATE]:
+            if tagtable[category].isnull().values.any():
+                st.warning(f"{category}: Contains Nulls - filling with empty string")
+                tagtable[category].fillna('', inplace=True)
             st.warning(f"{category}: Tags not found")
 
     if problem_counter == 0:
@@ -77,7 +84,7 @@ if folder:
         if st.sidebar.checkbox(f'Delete {category}'):
             delete_column(tagtable, category)
 
-    for category in ['bmp', 'media', 'organization']:
+    for category in ['bmp', 'media', 'organization', COMPOSER, 'conductor', 'lyricist', 'website', 'version']:
         if st.sidebar.checkbox(f'Update {category}'):
             update_based_on_previous_value(tagtable, category)
 
