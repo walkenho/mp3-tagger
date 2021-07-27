cd31858b964d6118435652f792b1c0f81b0dfaa5
Jessica Walkenhorst <walkenho@gmail.com>
2021-07-28 00:06:41 +0100

    Streamline interface creation

--- tagger.py
+++ tagger.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 from musictagger.core import MP3Table, set_mp3_coverart,\
-    DISCNUMBER, ALBUM, GENRE, ARTIST, ALBUMARTIST, ENCODEDBY, COPYRIGHT, \
-    TITLE, DATE, COMPOSER,\
-    TRACKNUMBER, LANGUAGE, LENGTH, BASEPATH
+    DISCNUMBER, ALBUM, GENRE, ARTIST, ALBUMARTIST, \
+    TITLE, DATE, TRACKNUMBER, LANGUAGE, BASEPATH
 from musictagger.interface import get_albumpath_from_interface, update_based_on_previous_value
 
 st.title("Welcome to the Music Tagger")
@@ -11,7 +10,8 @@ st.markdown("""## Instructions:
 1. Select the Albums you would like to retag.
 2. Select the procedures to apply from the sidebar.
 3. Check the results.
-4. If you are happy with the results, press Save.""")
+4. If you are happy with the results, press Save.
+5. If desired, select a cover image""")
 
 folder = get_albumpath_from_interface()
 if folder:
@@ -24,35 +24,26 @@ if folder:
     for category in [ALBUM, ARTIST, ALBUMARTIST, GENRE, LANGUAGE, DATE]:
         if category in mp3table.data.columns:
             if mp3table.data[category].isnull().values.any():
-                st.warning(f"{category}: Contains Nulls - filling with empty string")
-                mp3table.data[category].fillna('', inplace=True)
+                st.warning(f"{category}: Contains Nulls")
 
-            category_values = [str(v) for v in mp3table.data[category].unique()]
+            category_values = mp3table.data[category].dropna().astype('str').unique()
             if len(category_values) > 1:
                 st.warning(f"{category}: Multiple Entries found [{', '.join(category_values)}]")
                 problem_counter = problem_counter + 1
         else:
             st.warning(f"{category}: Tags not found")
 
-    #for category in mp3table.data.columns:
-    #    if category not in [ALBUM, ARTIST, ALBUMARTIST, GENRE, LANGUAGE, DATE]:
-            # this should work now that the delete option is there
-            #if mp3table.data[category].isnull().values.any():
-            #    st.warning(f"{category}: Contains Nulls - filling with empty string")
-            #    mp3table.data[category].fillna('', inplace=True)
-    # What was that supposed to do?
-    #        st.warning(f"{category}: Tags not found")
-
     if problem_counter == 0:
         st.success(f"No inconsistencies detected")
 
-    st.markdown("### Original Tags:")
+    st.header("Original Tags")
     st.write(mp3table.data)
 
     # Build sidebar menu
     st.sidebar.markdown("# Cleaning Tools")
-    st.sidebar.markdown("### Popular - Album/Track")
-    for category in [ALBUM, ARTIST, ALBUMARTIST, GENRE, LANGUAGE, DATE]:
+    st.sidebar.markdown("### Popular - Album/Multiple Tracks")
+    major_update_categories = [ALBUM, ARTIST, ALBUMARTIST, GENRE, LANGUAGE, DATE]
+    for category in major_update_categories:
         if category == LANGUAGE:
             options = ['english', 'spanish', 'deutsch', 'french']
         else:
@@ -74,26 +65,27 @@ if folder:
     if st.sidebar.checkbox(f'Cast {DISCNUMBER} to n/m format'):
         mp3table.set_combined_disc_number()
 
-    st.sidebar.markdown("### Popular - Track")
+    st.sidebar.markdown("### Popular - Single Track")
     if st.sidebar.checkbox(f'Set {TITLE}'):
         mp3table.data[TITLE] = st.sidebar.text_input(f"Set {TITLE}")
 
     if st.sidebar.checkbox(f'Set {TRACKNUMBER}'):
         mp3table.data[TRACKNUMBER] = st.sidebar.text_input(f"Set {TRACKNUMBER}")
 
-    st.sidebar.markdown("## Other")
-    for category in [LENGTH, COPYRIGHT, ENCODEDBY]:
-        if st.sidebar.checkbox(f'Delete {category}'):
-            mp3table.delete_tag(category)
+    st.sidebar.markdown("### Delete Tags")
+    tags_to_delete = st.sidebar.multiselect('Tags to delete (multi-choice)',
+                                            options=list(set(mp3table.data.columns).difference(['filename'])))
+    for category in tags_to_delete:
+        mp3table.delete_tag(category)
 
-    #TODO: Make this generated dynamically
-    for category in ['bmp', 'media', 'organization', COMPOSER, 'conductor', 'lyricist', 'website', 'version']:
+    st.sidebar.markdown("### Other")
+    for category in set(mp3table.data.columns)\
+            .difference(major_update_categories)\
+            .difference(['filename', TRACKNUMBER, DISCNUMBER]):
         if st.sidebar.checkbox(f'Update {category}'):
             update_based_on_previous_value(mp3table, category)
 
-    #TODO: Add delete option dynamically for all tags
-
-    st.markdown("### Updated Tags:")
+    st.header("Updated Tags")
     st.write(mp3table.data)
 
     save_button = st.button("Save")
