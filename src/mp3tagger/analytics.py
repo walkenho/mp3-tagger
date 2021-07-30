76d793531361480b37f0b940de67541fb19bef2e
Jessica Walkenhorst <walkenho@users.noreply.github.com>
2021-07-31 00:07:21 +0100

    Clean and add cleaning scripts (#7)
    Add cleaning scripts:
        bin/run-flake8.sh
        bin/run-black.sh
    
    Add minor code improvements

--- src/mp3tagger/analytics.py
+++ src/mp3tagger/analytics.py
@@ -1,13 +1,15 @@
-from datetime import datetime
 import json
+import logging
+from datetime import datetime
 from pathlib import Path
 from typing import List
-import pandas as pd
-from musictagger.core import find_all_mp3s, load_mp3, BASEPATH
-import logging
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-DATAFOLDER = Path().cwd().parent.parent/'analytics'
+import pandas as pd
+from musictagger.core import BASEPATH, find_all_mp3s, load_mp3
+
+logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+
+DATAFOLDER = Path().cwd().parent.parent / "analytics"
 
 
 def collect_data(path: Path):
@@ -24,12 +26,17 @@ def collect_data(path: Path):
             n_skipped = n_skipped + 1
         else:
             # convert path to str to be serializable later
-            tags.append({**{k: v[0] for k, v in audio.items()},
-                         'filename': str(filename)})
+            tags.append(
+                {**{k: v[0] for k, v in audio.items()}, "filename": str(filename)}
+            )
             if not audio:
                 n_untagged = n_untagged + 1
 
-    return tags, {'n_skipped': n_skipped, 'n_untagged': n_untagged, 'n_total': files_total}
+    return tags, {
+        "n_skipped": n_skipped,
+        "n_untagged": n_untagged,
+        "n_total": files_total,
+    }
 
 
 def filter_serializable_data(tags: List[dict]) -> List[dict]:
@@ -45,38 +52,42 @@ def filter_serializable_data(tags: List[dict]) -> List[dict]:
 
 
 def run_analytics():
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     results = []
     counters = []
 
-    with open(DATAFOLDER/f'details_{timestamp}.txt', 'w') as f:
+    with open(DATAFOLDER / f"details_{timestamp}.txt", "w") as f:
         # Go down one level to have more high-grain data
-        for entry in BASEPATH.glob('*'):
+        for entry in BASEPATH.glob("*"):
             if entry.is_dir():
                 f.writelines(f"Entering folder {entry}.\n")
                 tags, c = collect_data(entry)
                 results = results + tags
                 counters.append(c)
-                if c['n_skipped'] != 0:
+                if c["n_skipped"] != 0:
                     f.writelines(f"{c['n_skipped']} files skipped in this folder.\n")
-                if c['n_untagged'] !=0:
-                    f.writelines(f"{c['n_untagged']} files without tags in this folder.\n")
+                if c["n_untagged"] != 0:
+                    f.writelines(
+                        f"{c['n_untagged']} files without tags in this folder.\n"
+                    )
 
     df_counters = pd.DataFrame(counters)
 
-    with open(DATAFOLDER/f'summary_{timestamp}.txt', 'w') as f:
+    with open(DATAFOLDER / f"summary_{timestamp}.txt", "w") as f:
         f.writelines("======================\n")
         f.writelines(f"{df_counters['n_total'].sum()} mp3s analyzed\n")
         f.writelines(f"{len(results)} mp3s were added to table\n")
         f.writelines(f"{df_counters['n_skipped'].sum()} files skipped\n")
-        f.writelines(f"Out of the ingested, {df_counters['n_untagged'].sum()} completely untagged\n")
+        f.writelines(
+            f"Out of the ingested, {df_counters['n_untagged'].sum()} completely untagged\n"
+        )
 
     serializable_tags = filter_serializable_data(results)
 
-    with open(DATAFOLDER/f'data_{timestamp}.json', 'w', encoding='utf-8') as f:
+    with open(DATAFOLDER / f"data_{timestamp}.json", "w", encoding="utf-8") as f:
         json.dump(serializable_tags, f, ensure_ascii=False, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.info("Starting Analytics")
     run_analytics()
